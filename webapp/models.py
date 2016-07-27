966a0233be9fae9ff3bd40d73259188b084b9928
frenko <frenko@paranoici.org>
2016-07-27 19:25:00 +0200

    add model of db

--- webapp/models.py
+++ webapp/models.py
@@ -2,4 +2,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Opera(models.Model):
+	title = models.CharField(max_length=100)
+	title_original = models.CharField(max_length=100)
+	author = models.CharField(max_length=100)
+	period = models.CharField(max_length=100)
+	location = models.CharField(max_length=100)
+	writing_date = models.DateField()
+	description = models.TextField()
+	quote = models.TextField()
+	quote_original = models.TextField()
+	media_data = models.TextField()
+	pub_date = models.DateTimeField('date published')
