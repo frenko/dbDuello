dd40e3bbb56b3875cad424dc0722bde2acc5277f
Fred <fred@xps-box.station>
2016-08-21 14:03:39 +0200

    fix context_processors for update to django 1.10

--- dbDuello/settings.py
+++ dbDuello/settings.py
@@ -58,15 +58,17 @@ ROOT_URLCONF = 'dbDuello.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.core.context_processors.request',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+				'django.template.context_processors.debug',
+				'django.template.context_processors.i18n',
+				'django.template.context_processors.media',
+				'django.template.context_processors.static',
+				'django.template.context_processors.tz',
+				'django.contrib.messages.context_processors.messages',
             ],
         },
     },
