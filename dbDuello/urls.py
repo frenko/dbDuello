f2e9eb6638cd9256277046c41af20e9af2520a61
frenko@paranoici.org <frenko@paranoici.org>
2016-09-18 15:20:17 +0200

    preparing for merging

--- dbDuello/urls.py
+++ dbDuello/urls.py
@@ -24,8 +24,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login, {'template_name': 'webapp/registration/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
-    url(r'^password_reset/$', auth_views.password_reset, {'template_name': 'webapp/registration/password_change_form.html'}, name='password_reset'),
-    #url(r'^password_reset/done/$', auth_views.password_reset_done, {'template_name': 'webapp/registration/password_reset_done.html'}, name='password_reset_done'),
+    url(r'^password_reset/$', auth_views.password_reset, {'template_name': 'webapp/registration/password_reset_form.html'}, name='password_reset'),
     url('^', include('django.contrib.auth.urls')),
     url(r'^', include('registration.backends.simple.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
